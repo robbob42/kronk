ed980e07750da2aefb4996c24bc62013e751bf7a
Rob Olsen <robbob42@gmail.com>
2025-10-15 14:51:59 -0400

    Add email to user model

--- app/models.py
+++ app/models.py
@@ -13,6 +13,7 @@ completion_participants = db.Table('completion_participants',
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
     transactions = db.relationship('Transaction', backref='user', lazy='dynamic')
     is_active = db.Column(db.Boolean, default=True, nullable=False)
 
